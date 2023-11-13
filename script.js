c84b2ab53ae981b18b59107b9a018f4a1dc7a664
Mathews1004 <mathewsosbr@gmail.com>
2023-11-13 18:05:52 -0300

    add javascript

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ form.addEventListener('submit', handleSubmit);
 const inputValue = document.getElementById('value-real');
 const selectedCurrency = document.getElementById('currency');
 const result = document.getElementById('result');
+let valueConverted = 0;
 
 function handleSubmit(e) {
     e.preventDefault();
@@ -13,5 +14,37 @@ function handleSubmit(e) {
     return; 
     } else if(!selectedCurrency.value) {
         alert('Escolha uma moeda!')
+        return;
     }
+
+    converter()
 };
+
+function converter(){
+    if (selectedCurrency.value === 'eur') {
+        valueConverted = inputValue.value / 6.22;
+        result.innerHTML = valueFormatter('pt-BR', 'EUR');
+
+        animateResult();
+    } else if (selectedCurrency.value === 'dol'){
+        valueConverted = inputValue.value / 5.37;
+        result.innerHTML = valueFormatter('en-US', 'USD');
+
+        animateResult();
+    }
+
+    inputValue.value = '';
+    selectedCurrency.value = '';
+};
+
+function valueFormatter(locale, currency) {
+    const value = valueConverted.toLocaleString(`${locale}` , { style: 'currency', currency: `${currency}` });
+    return `<span>🤑</span> ${value} <span>🤑</span>`;
+};
+
+function animateResult() {
+    return result.animate([
+        { transform: 'translateY(-150px)'},
+        { transform: 'translateY(0px)'},
+    ], { duration: 500 });
+}
